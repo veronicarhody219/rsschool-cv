7577df21143d8b71d95fa6668d3fe79502b42d8b
Veronica Rhody <85674986+veronicarhody219@users.noreply.github.com>
2022-04-10 09:27:42 +0700

    feat: add rsschool-cv-html folder

---
+++ rsschool-cv-html/index.js
@@ -0,0 +1,70 @@
+// This code is commented
+
+// This code is commented */
+
+/* This code is commented */
+
+console.log(Number("123"));
+console.log(Number(0123));
+// alert('My awesome notification'); alert('My awesome notification as well');
+
+// alert(My awesome notification)
+
+// alert('My awesome notification')
+
+// console.log(10 > 100);
+// let testOption;
+// console.log(testOption);
+// console.log(String("123"));
+// console.log(123n + 345n);
+// console.log(`Secret code is ${12 * 5}`);
+// console.log("Secret code is ${12*5}");
+// console.log(typeof 0);
+
+// let 1_floor;
+
+// let floor_1;
+
+// let _ = 'ooops';
+
+// let $ = 'blah-blah';
+
+// let newVar;
+
+// let new = 'new option'
+
+console.log(Boolean("0"));
+let whatIsIt;
+console.log(whatIsIt);
+
+// const myName = "Barbie";
+
+// const myName = 'Mary";
+
+// const myName = 'Bob';
+
+// const myName = `Joe`;
+// const myArr = []
+// myArr.push(myName)
+// console.log(myArr);
+
+// console.log(0 / 10);
+// console.log(10 / 0);
+
+const mySize = "M";
+
+// const mySize = "M";
+// const mySize = "L";
+
+const MY_SIZE = "M";
+
+// let mySize = "L";
+// const mySize = "M";
+
+// console.log(Boolean(1));
+
+// let testOption;
+// testOption = "i set the value"
+// testOption = 'i updated the value'
+// alert(testOption);
+console.log(typeof 10n);
